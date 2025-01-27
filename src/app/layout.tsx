ec11467b318f163c18b28531df6aadc5f8fe67cc
GwangMyeong Seo <auddlzz000@gmail.com>
2025-01-27 05:45:15 +0000

    prettier 자동 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css';
+import type { Metadata } from "next";
+import "./globals.css";
 import Header from "@/shared/ui/Header";
-import Footer from '@/shared/ui/Footer';
+import Footer from "@/shared/ui/Footer";
 
 export const metadata: Metadata = {
-    title: '냉장고마스터',
-    description: '스마트 요리 비서',
+  title: "냉장고마스터",
+  description: "스마트 요리 비서",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="ko">
-            <body>
-                <Header />
-                <main>{children}</main>
-                <Footer />
-            </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>냉장고마스터: 스마트 요리 비서</h1>
       <p>당신의 냉장고 재료로 최고의 요리를 만들어보세요!</p>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
