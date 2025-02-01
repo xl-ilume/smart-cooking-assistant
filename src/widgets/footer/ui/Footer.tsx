59aa139bf55ecbf0b5f2856abbe05281ba5d2998
GwangMyeong Seo <auddlzz000@gmail.com>
2025-02-01 04:30:06 +0000

    footer 메뉴가 메인컴포넌트에 가려지는 이슈 수정

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,19 +1,16 @@
 "use client";
 
+import Link from "next/link";
 import { Home, BookOpen, List, Star, User } from "lucide-react";
-import NavItem from "./NavItem"; // 같은 ui 폴더 내에서 가져오기
+import NavItem from "./NavItem";
 
 export default function Footer() {
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-md">
+    <nav className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-md z-10"> 
       <div className="max-w-[1024px] mx-auto flex justify-around py-3">
         <NavItem href="/" icon={<Home size={24} />} label="홈" />
         <NavItem href="/recipes" icon={<BookOpen size={24} />} label="레시피" />
-        <NavItem
-          href="/ingredients"
-          icon={<List size={24} />}
-          label="재료관리"
-        />
+        <NavItem href="/ingredients" icon={<List size={24} />} label="재료관리" />
         <NavItem href="/favorites" icon={<Star size={24} />} label="즐겨찾기" />
         <NavItem href="/mypage" icon={<User size={24} />} label="마이페이지" />
       </div>
